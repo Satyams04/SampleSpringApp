98e6a2cc74fc92b577d565dff6cf5089a568ae37
Satyam Soni <satyamsoni@github.com>
2021-05-25 19:16:43 +0530

    SampleSpringApp

---
+++ src/test/java/com/satyam/SampleApp/SampleAppTests.java
@@ -0,0 +1,13 @@
+package com.shashi.App31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
